5d474d829ccdb0130235455a3557d45104b0dc3b
Timothy <dinosauli2006@gmail.com>
2026-02-07 21:38:48 -0500

    fix cors

--- src/graphql.ts
+++ src/graphql.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
       logger: ['error', 'warn'],
     })
     app.enableCors({
-      origin: 'https://stripelearning.vercel.app',
+      origin: ['https://demoecommerces.vercel.app', 'https://stripelearning.vercel.app'],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       allowedHeaders: 'X-Requested-With, Content-Type, Accept, Authorization',
       credentials: true,
